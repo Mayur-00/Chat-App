4ae1c73501f9144a3b6a0b4f81e3c44f9e9dd51a
mayur-000 <kendremayur31@gmail.com>
2024-12-05 12:16:40 +0530

    app completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,12 @@
 import express from"express";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import cors from "cors"
-
-import dotenv from "dotenv"
-
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import { app, server,  } from "./lib/socket.js";
+import path from "path";
 
 
 dotenv.config()
@@ -18,14 +17,21 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
-const port =process.env.PORT || 5001
+const port =process.env.PORT || 5001;
+const __dirname= path.resolve();
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")))
+};
 
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
+})
 
 connectDB()
 .then(() => {
